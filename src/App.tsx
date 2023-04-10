37ea949bc43790c256cff7711048efafcfd9dec5
Murtuzaali Surti <murtuzaali.surti@imriel.com>
2023-04-10 11:35:59 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  const main = (
+    <main className="contain">
+      hello
+    </main>
+  )
+  console.log(main);
+  return main
+}
+
+export default App
